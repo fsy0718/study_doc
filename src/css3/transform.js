c183d7d50af6205063ef02f2b5ce074f110fde8c
fsy0718 <aw_fanshaoyong@163.com>
2015-07-09 22:17:52 +0800

    ifxed

---
+++ src/css3/transform.js
@@ -0,0 +1,9 @@
+/**
+ * @module transform
+ * @description 变行
+ - 浏览器支持情况
+  <strong class="iconfont browser" data-browser="ie" data-version="9+">IE</strong><strong class="iconfont browser" data-browser="firefox" data-version="3.5+">Firefox</strong><strong class="iconfont browser" data-browser="chrome">Chrome</strong><strong class="iconfont browser" data-browser="opera" data-version="10.5+">Opera</strong><strong class="iconfont browser" data-browser="safari" data-version="3.1+">Safari</strong>
+ */
+module.exports = {
+
+}
