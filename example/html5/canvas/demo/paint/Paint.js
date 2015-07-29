d431487cf92087c54f8ccc6510b42ef3017b4b79
fsy0718 <fsy0718@sxdoo.com>
2015-07-29 09:56:00 +0800

    fixed

--- example/html5/canvas/demo/paint/Paint.js
+++ example/html5/canvas/demo/paint/Paint.js
@@ -4,7 +4,11 @@ define(['./transformPosToWindow'],function(transformPosToWindow){
     height: 800
   };
 
-  var _eventContext,mouseInfos = {},isDragging = false,imageData = [];
+  var _eventContext,mouseInfos = {},isDragging = false;window.imageData = [];
+
+  var isFunction = function(fn){
+    return Object.prototype.toString.call(fn) === '[object Function]';
+  }
 
   var  initPaint = function(context){
     context.cvs.width = paintMap.width;
@@ -12,35 +16,56 @@ define(['./transformPosToWindow'],function(transformPosToWindow){
   }
 
   var saveDrawingSurface = function(context,idx){
-    context.putImageData(context.ctx.getImageData(0,0,paintMap.width,paintMap.height),idx);
+    idx = idx || imageData.length;
+    if(idx > 9){
+      imageData.shift();
+      idx = imageData.length;
+    }
+    context.setImageData(context.ctx.getImageData(0,0,paintMap.width,paintMap.height),idx);
   }
 
   var restoreDrawingSurface = function(context,idx){
+    idx = +idx ? +idx : imageData.length - 1;
+    if(idx >= imageData.length){
+      idx = imageData.length - 1;
+    }
+    idx = idx ? (idx < 0 ? 0 : idx) : 0;
     var data = context.getImageData(idx);
     context.ctx.putImageData(data,0,0);
   }
 
   var mousedownHandler = function(e){
-
+    e.preventDefault();
     if(_eventContext){
       var pos = transformPosToWindow(e,_eventContext.cvs)
       mouseInfos.x = pos.x;
       mouseInfos.y = pos.y;
       isDragging = true;
-      _eventContext.ctx.moveTo(mouseInfos.x,mouseInfos.y);
+      saveDrawingSurface(_eventContext);
+      isFunction(_eventContext.mousedownPaint) && _eventContext.mousedownPaint(pos,e,_eventContext);
     }
 
   }
 
   var mousemoveHandler = function(e){
+    e.preventDefault();
     if(_eventContext && isDragging){
       var pos  = transformPosToWindow(e,_eventContext.cvs);
+      restoreDrawingSurface(_eventContext);
+      isFunction(_eventContext.mousemovePaint && _eventContext.mousemovePaint(pos,e,_eventContext));
       _eventContext.owner.draw(mouseInfos,pos);
     }
 
   }
   var mouseupHandler = function(e){
-
+    e.preventDefault();
+    if(_eventContext && isDragging){
+      var pos = transformPosToWindow(e,_eventContext.cvs);
+      var idx = imageData.length;
+      saveDrawingSurface(_eventContext,idx-1);
+      isDragging = false;
+      _eventContext = null;
+    }
   }
 
   var initEvent = function(context){
@@ -64,7 +89,8 @@ define(['./transformPosToWindow'],function(transformPosToWindow){
     return imageData[idx];
   }
 
-  Paint.prototype.putImageData = function(data,idx){
+  Paint.prototype.setImageData = function(data,idx){
+    console.log(data,idx);
     return imageData[idx] = data;
   }
 
